eb2908224dc7ca39381ead8ba501a170bd728cf0
AnoukSmet <smet.anouk@gmail.com>
2020-12-07 13:51:09 +0000

    Added basic layout for navigation bar

--- casa_pedra_nobre/settings.py
+++ casa_pedra_nobre/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -147,4 +148,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = ('/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
