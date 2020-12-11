5d3118e55ec6680d75db0298873a3b53c3a3ffd9
AnoukSmet <smet.anouk@gmail.com>
2020-12-11 10:50:05 +0000

    Added Recommendation Model and Recommendation Category and gave personalised admin

--- tourist_info/admin.py
+++ tourist_info/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from .models import Recommendation, RecommendationCategory
 # Register your models here.
+
+
+class RecommendationAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'intro',
+        'category',
+    )
+
+    ordering = ('category',)
+
+
+class RecommendationCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Recommendation, RecommendationAdmin)
+admin.site.register(RecommendationCategory, RecommendationCategoryAdmin)
--- tourist_info/models.py
+++ tourist_info/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class RecommendationCategory(models.Model):
+    class Meta:
+        verbose_name_plural = 'RecommendationCategories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+
+class Recommendation(models.Model):
+    category = models.ForeignKey('RecommendationCategory',
+                                 blank=True, null=True,
+                                 on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    image = models.ImageField()
+    intro = models.TextField(max_length=254)
+    description = models.TextField()
+    link_to_website = models.URLField()
+    link_to_google_maps = models.URLField()
+    distance = models.DecimalField(max_digits=6, decimal_places=2)
+    favorite = models.ManyToManyField(
+        User, related_name='favorite', blank=True)
+
+    def __str__(self):
+        return self.name
