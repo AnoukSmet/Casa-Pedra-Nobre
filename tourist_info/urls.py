1f111108b342d823e10c9aa857619853d474e9f7
AnoukSmet <smet.anouk@gmail.com>
2020-12-14 10:15:58 +0000

    Added recommendation detail page, view and basic styling

--- tourist_info/urls.py
+++ tourist_info/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.tourist_info, name="tourist_info"),
+    path('tourist-info/<int:recommendation_id>', views.recommendation_detail, name="recommendation_detail"),
 ]
--- tourist_info/views.py
+++ tourist_info/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Recommendation, RecommendationCategory
 # Create your views here.
 
@@ -11,6 +11,17 @@ def tourist_info(request):
     context = {
         'recommendations': recommendations,
         'categories': categories,
+    }
+
+    return render(request, template, context)
+
+
+def recommendation_detail(request, recommendation_id):
+    recommendation = get_object_or_404(Recommendation, pk=recommendation_id)
+
+    template = 'tourist_info/recommendation_detail.html'
+    context = {
+        'recommendation': recommendation,
     }
 
     return render(request, template, context)
