4cced4188b0eb029fa9a4989acd4269d137690a2
AnoukSmet <smet.anouk@gmail.com>
2020-12-14 11:18:53 +0000

    Finalised styling recommendationdetail and added add to favorites functionality

--- tourist_info/urls.py
+++ tourist_info/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.tourist_info, name="tourist_info"),
     path('tourist-info/<int:recommendation_id>', views.recommendation_detail, name="recommendation_detail"),
+    path('add-to-favorites/<recommendation_id>/', views.add_to_favorites, name='add_to_favorites'),
 ]
--- tourist_info/views.py
+++ tourist_info/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, reverse
 from .models import Recommendation, RecommendationCategory
 # Create your views here.
 
@@ -18,10 +18,23 @@ def tourist_info(request):
 
 def recommendation_detail(request, recommendation_id):
     recommendation = get_object_or_404(Recommendation, pk=recommendation_id)
-
+    is_favorite = False
+    if recommendation.favorite.filter(id=request.user.id).exists():
+        is_favorite = True
     template = 'tourist_info/recommendation_detail.html'
     context = {
         'recommendation': recommendation,
+        'is_favorite': is_favorite,
     }
 
     return render(request, template, context)
+
+
+def add_to_favorites(request, recommendation_id):
+    recommendation = get_object_or_404(Recommendation, id=recommendation_id)
+    if recommendation.favorite.filter(id=request.user.id).exists():
+        recommendation.favorite.remove(request.user)
+    else:
+        recommendation.favorite.add(request.user)
+    return HttpResponseRedirect(reverse(
+        'recommendation_detail', kwargs={'recommendation_id': recommendation_id}))
