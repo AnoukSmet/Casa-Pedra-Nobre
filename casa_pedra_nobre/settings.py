004f59c21a77f2f1e08560b82c2396922d3ac7fe
AnoukSmet <smet.anouk@gmail.com>
2020-12-29 11:37:02 +0000

    Updated import SECRET KEY in settings.py

--- casa_pedra_nobre/settings.py
+++ casa_pedra_nobre/settings.py
@@ -13,15 +13,15 @@ import os
 from pathlib import Path
 import dj_database_url
 if os.path.exists('env.py'):
-	import env
+    import env
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", '')
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
