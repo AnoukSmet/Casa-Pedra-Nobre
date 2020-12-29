f0094b47de1d302bf1a22c21bc6ccea8e5123f03
AnoukSmet <smet.anouk@gmail.com>
2020-12-29 13:09:18 +0000

    Loaded data to POSTGRES

--- casa_pedra_nobre/settings.py
+++ casa_pedra_nobre/settings.py
@@ -138,17 +138,17 @@ WSGI_APPLICATION = 'casa_pedra_nobre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if 'DATABASE_URL' in os.environ:
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 # Password validation
