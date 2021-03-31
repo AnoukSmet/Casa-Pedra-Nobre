6360649cfa3ab77b4cfa48384ff659e73579a272
AnoukSmet <smet.anouk@gmail.com>
2021-03-31 14:11:35 +0000

    Added cache control

--- casa_pedra_nobre/settings.py
+++ casa_pedra_nobre/settings.py
@@ -192,10 +192,17 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = ('/media/')
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'casa-pedra-nobre'
     AWS_S3_REGION_NAME = "eu-central-1"
